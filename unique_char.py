c3203f1011409c53699f32179cf74cba883ecbf9
kostyakravchenko <dev.java.online@gmail.com>
2023-07-18 17:27:08 +0300

    add .gitignore

--- unique_char.py
+++ unique_char.py
@@ -1,20 +1,4 @@
-__all__ = ['find_unique_ch', 'find_unique_character']
-
 import re
-
-text = """
-The Tao gave birth to machine language.  Machine language gave birth
-to the assembler.
-The assembler gave birth to the compiler.  Now there are ten thousand
-languages.
-Each language has its purpose, however humble.  Each language
-expresses the Yin and Yang of software.  Each language has its place within
-the Tao.
-But do not program in COBOL if you can avoid it.
-        -- Geoffrey James, "The Tao of Programming"
-"""
-
-text = "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup R# R++"
 
 
 def find_unique_character(s):
