875c593700e9fd6b3708cf4818a8843406ce489b
kostyakravchenko <dev.java.online@gmail.com>
2023-07-18 17:22:59 +0300

    add .gitignore

--- unique_char_test.py
+++ unique_char_test.py
@@ -1,10 +1,12 @@
-from unique_char import find_unique_ch
-
 import unittest
 
-test_data = [["e",
-              "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup R# R++"],
-             ["m", "The Tao gave birth to machine language.  Machine language gave birth\n" +
+import pytest
+
+from unique_char import find_unique_ch
+
+test_data = [("e",
+              "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup R# R++"),
+             ("m", "The Tao gave birth to machine language.  Machine language gave birth\n" +
               "to the assembler.\n" +
               "The assembler gave birth to the compiler.  Now there are ten thousand\n" +
               "languages.\n" +
@@ -12,20 +14,19 @@ test_data = [["e",
               "expresses the Yin and Yang of software.  Each language has its place within\n" +
               "the Tao.\n" +
               "But do not program in COBOL if you can avoid it.\n" +
-              "        -- Geoffrey James, \"The Tao of Programming\""],
-             ["", ""],
-             ["1", "1"],
-             ["W", "QQQ EWE TTT"],
-             ["", "QWQ EWE TWT"],
-             ["H", "Hello World"],
-             ["r", "bbookkeepper"],
-             ["T", "Testing"]]
+              "        -- Geoffrey James, \"The Tao of Programming\""),
+             ("", ""),
+             ("1", "1"),
+             ("W", "QQQ EWE TTT"),
+             ("", "QWQ EWE TWT"),
+             ("H", "Hello World"),
+             ("r", "bbookkeepper"),
+             ("T", "Testing")]
 
 
-class Testing(unittest.TestCase):
-    def test_unique_char(self):
-        for case in test_data:
-            self.assertEqual(case[0], find_unique_ch(case[1]))
+@pytest.mark.parametrize("expected, test_input", test_data)
+def test_eval(expected, test_input):
+    assert find_unique_ch(test_input) == expected
 
 
 if __name__ == '__main__':
